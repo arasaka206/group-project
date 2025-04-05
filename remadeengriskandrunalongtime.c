572e3b52e197779bf8afe2c5fed4778a38f834d9
Trần Mạnh Dũng <146959954+arasaka206@users.noreply.github.com>
2025-04-06 06:07:59 +0700

    Ông tu update remadeengriskandrunalongtime.c

--- remadeengriskandrunalongtime.c
+++ remadeengriskandrunalongtime.c
@@ -5,20 +5,153 @@
 #define MAX_LEN 100
 #define SHIFT 3
 
-void caesarEncrypt(char *str) {
-    int i = 0;
-    while (str[i] != '\0' && str[i] != '\n') {
-        str[i] += SHIFT;
-        i++;
-    }
-}
+#define BYTE_RANGE 256
+const char *key_file = "key.txt";
+char *encrypt(const char *password, const char *key_file_path) {
+    static char hex_output[512]; 
+    unsigned char output[256];
+    unsigned char rotor1[BYTE_RANGE], rotor2[BYTE_RANGE], rotor3[BYTE_RANGE];
+    unsigned char reverse1[BYTE_RANGE], reverse2[BYTE_RANGE], reverse3[BYTE_RANGE];
+    unsigned char reflector[BYTE_RANGE], plugboard[BYTE_RANGE];
+    int pos1 = 0, pos2 = 0, pos3 = 0;
 
-void caesarDecrypt(char *str) {
-    int i = 0;
-    while (str[i] != '\0' && str[i] != '\n') {
-        str[i] -= SHIFT;
-        i++;
+    FILE *file = fopen(key_file_path, "r");
+    if (!file) return NULL;
+
+    unsigned int seed;
+    if (fscanf(file, "%u", &seed) != 1) {
+        fclose(file);
+        return NULL;
     }
+    fclose(file);
+
+    srand(seed);
+
+    for (int i = 0; i < BYTE_RANGE; i++) {
+        rotor1[i] = rotor2[i] = rotor3[i] = reflector[i] = plugboard[i] = i;
+    }
+    for (int i = BYTE_RANGE - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        unsigned char tmp;
+
+        tmp = rotor1[i]; rotor1[i] = rotor1[j]; rotor1[j] = tmp;
+        tmp = rotor2[i]; rotor2[i] = rotor2[j]; rotor2[j] = tmp;
+        tmp = rotor3[i]; rotor3[i] = rotor3[j]; rotor3[j] = tmp;
+        tmp = reflector[i]; reflector[i] = reflector[j]; reflector[j] = tmp;
+    }
+
+    for (int i = 0; i < BYTE_RANGE; i++) {
+        reverse1[rotor1[i]] = i;
+        reverse2[rotor2[i]] = i;
+        reverse3[rotor3[i]] = i;
+    }
+
+    size_t len = strlen(password);
+    for (size_t i = 0; i < len; i++) {
+        pos1 = (pos1 + 1) % BYTE_RANGE;
+        if (pos1 == 0) {
+            pos2 = (pos2 + 1) % BYTE_RANGE;
+            if (pos2 == 0) {
+                pos3 = (pos3 + 1) % BYTE_RANGE;
+            }
+        }
+
+        unsigned char b = (unsigned char)password[i];
+
+        b = plugboard[b];
+        b = rotor1[(b + pos1) % BYTE_RANGE];
+        b = rotor2[(b + pos2) % BYTE_RANGE];
+        b = rotor3[(b + pos3) % BYTE_RANGE];
+        b = reflector[b];
+        b = (reverse3[b] + BYTE_RANGE - pos3) % BYTE_RANGE;
+        b = (reverse2[b] + BYTE_RANGE - pos2) % BYTE_RANGE;
+        b = (reverse1[b] + BYTE_RANGE - pos1) % BYTE_RANGE;
+        b = plugboard[b];
+
+        output[i] = b;
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        sprintf(hex_output + i * 2, "%02X", output[i]);
+    }
+    hex_output[len * 2] = '\0';
+
+    return hex_output;
+}
+char *decrypt(const char *encrypted_password, const char *key_file_path) {
+    static char hex_output[512]; 
+    unsigned char output[256];
+    unsigned char rotor1[BYTE_RANGE], rotor2[BYTE_RANGE], rotor3[BYTE_RANGE];
+    unsigned char reverse1[BYTE_RANGE], reverse2[BYTE_RANGE], reverse3[BYTE_RANGE];
+    unsigned char reflector[BYTE_RANGE], plugboard[BYTE_RANGE];
+    int pos1 = 0, pos2 = 0, pos3 = 0;
+
+    FILE *file = fopen(key_file_path, "r");
+    if (!file) return NULL;
+
+    unsigned int seed;
+    if (fscanf(file, "%u", &seed) != 1) {
+        fclose(file);
+        return NULL;
+    }
+    fclose(file);
+
+    srand(seed);
+
+    for (int i = 0; i < BYTE_RANGE; i++) {
+        rotor1[i] = rotor2[i] = rotor3[i] = reflector[i] = plugboard[i] = i;
+    }
+    for (int i = BYTE_RANGE - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        unsigned char tmp;
+
+        tmp = rotor1[i]; rotor1[i] = rotor1[j]; rotor1[j] = tmp;
+        tmp = rotor2[i]; rotor2[i] = rotor2[j]; rotor2[j] = tmp;
+        tmp = rotor3[i]; rotor3[i] = rotor3[j]; rotor3[j] = tmp;
+        tmp = reflector[i]; reflector[i] = reflector[j]; reflector[j] = tmp;
+    }
+
+    for (int i = 0; i < BYTE_RANGE; i++) {
+        reverse1[rotor1[i]] = i;
+        reverse2[rotor2[i]] = i;
+        reverse3[rotor3[i]] = i;
+    }
+
+    size_t len = strlen(encrypted_password) / 2;
+    for (size_t i = 0; i < len; i++) {
+        sscanf(encrypted_password + i * 2, "%2hhX", &output[i]);
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        unsigned char b = output[i];
+
+        pos1 = (pos1 + 1) % BYTE_RANGE;
+        if (pos1 == 0) {
+            pos2 = (pos2 + 1) % BYTE_RANGE;
+            if (pos2 == 0) {
+                pos3 = (pos3 + 1) % BYTE_RANGE;
+            }
+        }
+
+        b = plugboard[b];
+        b = rotor1[(b + pos1) % BYTE_RANGE];
+        b = rotor2[(b + pos2) % BYTE_RANGE];
+        b = rotor3[(b + pos3) % BYTE_RANGE];
+        b = reflector[b];
+        b = (reverse3[b] + BYTE_RANGE - pos3) % BYTE_RANGE;
+        b = (reverse2[b] + BYTE_RANGE - pos2) % BYTE_RANGE;
+        b = (reverse1[b] + BYTE_RANGE - pos1) % BYTE_RANGE;
+        b = plugboard[b];
+
+        output[i] = b;
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        hex_output[i] = (char)output[i];
+    }
+    hex_output[len] = '\0';
+
+    return hex_output;
 }
 
 void exit_program() {
@@ -49,16 +182,15 @@ int main() {
             printf("Create your password (no spaces): ");
             scanf("%s", password);
 
-            sprintf(filename, "%s_wallet.txt", username);
-
             fp = fopen(filename, "w");
             if (fp == NULL) {
                 printf("Error creating file.\n");
                 continue;
             }
 
-            caesarEncrypt(password);
-            fprintf(fp, "PASSWORD:%s\n", password);
+            char* encrypted_password = encrypt(password, key_file);
+            sprintf(filename, "%s_wallet.txt", username);
+            fprintf(fp, "PASSWORD:%s\n", encrypted_password);
             fclose(fp);
             printf("Account created successfully!\n");
 
@@ -78,9 +210,9 @@ int main() {
 
             char storedPass[MAX_LEN];
             fscanf(fp, "PASSWORD:%s\n", storedPass);
-            caesarDecrypt(storedPass);
+            char* decrypted_password = decrypt(storedPass, "key.txt");
 
-            if (strcmp(storedPass, inputPass) == 0) {
+            if (strcmp(decrypted_password, inputPass) == 0) {
                 printf("Login successful!\n");
                 loggedIn = 1;
             } else {
@@ -105,8 +237,7 @@ int main() {
                     printf("Enter new password: ");
                     scanf("%s", newPass);
 
-                    caesarEncrypt(newUser);
-                    caesarEncrypt(newPass);
+                    char* newPass_encrypted = encrypt(newPass, key_file);
 
                     fp = fopen(filename, "a");
                     if (fp == NULL) {
@@ -115,7 +246,7 @@ int main() {
                     }
 
                     fprintf(fp, "USER:%s\n", newUser);
-                    fprintf(fp, "PASS:%s\n", newPass);
+                    fprintf(fp, "PASS:%s\n", newPass_encrypted);
                     fclose(fp);
 
                     printf("Credentials added.\n");
@@ -134,8 +265,8 @@ int main() {
                     printf("\n--- Wallet Contents ---\n");
 
                     while (fscanf(fp, "%[^:]:%s\n", label, value) != EOF) {
-                        caesarDecrypt(value);
-                        printf("%s: %s\n", label, value);
+                        char* decrypted_value = decrypt(value,"key.txt");
+                        printf("%s: %s\n", label, decrypted_value);
                     }
 
                     fclose(fp);
